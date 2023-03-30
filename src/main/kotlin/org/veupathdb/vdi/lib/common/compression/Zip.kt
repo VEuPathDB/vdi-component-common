b6af3d01b01cc59f2a907b8b94087bb386273c5b
Elizabeth <foxcapade@gmail.com>
2023-03-30 15:57:03 -0400

    unzip method

--- src/main/kotlin/org/veupathdb/vdi/lib/common/compression/Zip.kt
+++ src/main/kotlin/org/veupathdb/vdi/lib/common/compression/Zip.kt
@@ -2,8 +2,9 @@ package org.veupathdb.vdi.lib.common.compression
 
 import java.nio.file.Path
 import java.nio.file.StandardOpenOption
-import kotlin.io.path.inputStream
-import kotlin.io.path.isRegularFile
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
+import kotlin.io.path.*
 
 object Zip {
   private val SingleZipHeader  = byteArrayOf(0x50, 0x4B, 0x03, 0x04)
@@ -39,6 +40,68 @@ object Zip {
       else
         ZipType.Invalid
     }
+  }
+
+  /**
+   * Unzips the target path into the given [into] directory.
+   *
+   * @receiver Path to the zip file to unzip.
+   *
+   * @param into Directory into which the zip file contents should be unzipped.
+   *
+   * **NOTE** This directory must exist and be empty at the time of this method
+   * call.
+   *
+   * @return A collection of paths to the files that were unzipped.
+   */
+  fun Path.unzip(into: Path): Collection<Path> {
+    if (!into.exists())
+      throw IllegalStateException("cannot unzip $this into non-existent path $into")
+
+    if (!into.isDirectory())
+      throw IllegalStateException("cannot unzip $this into non-directory path $into")
+
+    if (!exists())
+      throw IllegalStateException("cannot unzip non-existent file $this")
+
+    if (!isRegularFile())
+      throw IllegalStateException("cannot unzip non-regular-file $this")
+
+    if (!isReadable())
+      throw IllegalStateException("cannot unzip unreadable file $this")
+
+    if (into.listDirectoryEntries().isNotEmpty())
+      throw IllegalStateException("refusing to unzip $this into non-empty directory $into")
+
+    val unzipped = ArrayList<Path>(2)
+
+    inputStream().use { rawInput ->
+      val archiveInput = ZipInputStream(rawInput)
+
+      var entry: ZipEntry?
+
+      while (true) {
+        entry = archiveInput.nextEntry
+
+        if (entry == null)
+          break
+
+        val target = into.resolve(entry.name)
+
+        if (entry.isDirectory) {
+          target.createDirectories()
+        } else {
+          if (target.parent != into)
+            target.parent.createDirectories()
+
+          target.outputStream().use { outStream -> archiveInput.transferTo(outStream) }
+
+          unzipped.add(target)
+        }
+      }
+    }
+
+    return unzipped
   }
 }
 
