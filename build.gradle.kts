2177032210d5e43c6c8855e8190395b9ea6abf12
Elizabeth <foxcapade@gmail.com>
2023-03-30 10:25:38 -0400

    move compression into the commons package

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,8 @@ dependencies {
 
   testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
   testImplementation("org.mockito:mockito-core:5.2.0")
-  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")}
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+}
 
 tasks.test {
   useJUnitPlatform()
