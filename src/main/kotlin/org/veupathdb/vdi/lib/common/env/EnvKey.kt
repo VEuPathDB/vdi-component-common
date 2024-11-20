ccee2599a5bd1cdcefc91b4bd9e7302de656184b
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2024-11-20 12:23:15 -0500

    add common prefix to appdb env var names

--- src/main/kotlin/org/veupathdb/vdi/lib/common/env/EnvKey.kt
+++ src/main/kotlin/org/veupathdb/vdi/lib/common/env/EnvKey.kt
@@ -79,70 +79,72 @@ object EnvKey {
    */
   object AppDB {
 
+    const val CommonPrefix = "DB_CONNECTION_"
+
     /**
      * Type: Boolean
      * Required: no
      */
-    const val DBEnabledPrefix = "DB_CONNECTION_ENABLED_"
+    const val DBEnabledPrefix = "${CommonPrefix}ENABLED_"
 
     /**
      * Type: String
      * Required: no
      */
-    const val DBHostPrefix = "DB_CONNECTION_HOST_"
+    const val DBHostPrefix = "${CommonPrefix}HOST_"
 
     /**
      * Type: UByte
      * Required: no
      */
-    const val DBPortPrefix = "DB_CONNECTION_PORT_"
+    const val DBPortPrefix = "${CommonPrefix}PORT_"
 
     /**
      * Type: String
      * Required: yes
      */
-    const val DBControlSchemaPrefix = "DB_CONNECTION_CONTROL_SCHEMA_"
+    const val DBControlSchemaPrefix = "${CommonPrefix}CONTROL_SCHEMA_"
 
     /**
      * Type: String
      * Required: yes
      */
-    const val DBDataSchemaPrefix = "DB_CONNECTION_DATA_SCHEMA_"
+    const val DBDataSchemaPrefix = "${CommonPrefix}DATA_SCHEMA_"
 
     /**
      * Type: String
      * Required: yes
      */
-    const val DBConnectionNamePrefix = "DB_CONNECTION_NAME_"
+    const val DBConnectionNamePrefix = "${CommonPrefix}NAME_"
 
     /**
      * Type: String
      * Required: yes
      */
-    const val DBLDAPPrefix = "DB_CONNECTION_LDAP_"
+    const val DBLDAPPrefix = "${CommonPrefix}LDAP_"
 
     /**
      * Type: String
      * Required: yes
      */
-    const val DBPassPrefix = "DB_CONNECTION_PASS_"
+    const val DBPassPrefix = "${CommonPrefix}PASS_"
 
     /**
      * Type: UByte
      * Required: no
      */
-    const val DBPoolPrefix = "DB_CONNECTION_POOL_SIZE_"
+    const val DBPoolPrefix = "${CommonPrefix}POOL_SIZE_"
 
     /**
      * Type: String
      * Required: no
      */
-    const val DBPlatformPrefix = "DB_CONNECTION_PLATFORM_"
+    const val DBPlatformPrefix = "${CommonPrefix}PLATFORM_"
 
     /**
      * Type: String
      * Required: no
      */
-    const val DBNamePrefix = "DB_CONNECTION_DB_NAME_"
+    const val DBNamePrefix = "${CommonPrefix}DB_NAME_"
   }
 }
